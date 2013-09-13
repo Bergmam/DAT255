a87c8bccd4ba9b84817888215f97080607b4ea0f
Karamech <emma@goding.eu>
2013-09-13 15:28:31 +0200

    Added methods and javadoc in GameInterface

--- dat255/src/model/GameInterface.java
+++ dat255/src/model/GameInterface.java
@@ -1,20 +1,58 @@
 package model;
 
+/**
+ * Interface for top class in the game risk
+ * 
+ * @author Emma Håkansson
+ * @since 2013-09-13
+ *
+ */
 public interface GameInterface {
 	
+	/**
+	 * Method for changing the state of the game to 
+	 * the next state if it should be changed.
+	 */
 	public void changePhase();
 	
+	/**
+	 * Method for changing the turn to the next player in turn.
+	 */
 	public void changeTurn();
 	
-	public void attack(Province offensiv, Province defensive);
 	
 	public Player getActivePlayer();
 	
+	/**
+	 * Method for handeling going in to battle and register the result of the battle.
+	 * @param offensiv, the attacking province. 
+	 * @param defensive, the province being attacked.
+	 */
+	public void attack(Province offensiv, Province defensive);
+	
+	/**
+	 * Method for handing a card from the deck to the active player.
+	 */
 	public void dealCard();
 	
-	public int calcBonus();
+	/**
+	 * Method for calculating the amount of units
+	 * the player will receive at the start of his turn.
+	 * @return the amount of units the player may place.
+	 */
+	public int calcBonusUnits();
 	
-	public void placeUnits();
+	/**
+	 * Method for placing the amount of units the player chooses
+	 * the place on the province the player chooses to place them.
+	 * @param units, the number of units being placed
+	 */
+	public void placeBonusUnits(int units, Province province);
 	
+	/**
+	 * Method for retrieving the number of units the player has left to place.
+	 * @return The number of units left.
+	 */
+	public int getBonusUnitsLeft();
 
 }
