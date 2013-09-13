b1340ed6d8b3749f002a02d040392eddc7cf573b
Karamech <emma@goding.eu>
2013-09-13 11:15:12 +0200

    Added methods to GameInterface

--- dat255/src/model/GameInterface.java
+++ dat255/src/model/GameInterface.java
@@ -1,5 +1,20 @@
 package model;
 
 public interface GameInterface {
+	
+	public void changePhase();
+	
+	public void changeTurn();
+	
+	public void attack(Province offensiv, Province defensive);
+	
+	public Player getActivePlayer();
+	
+	public void dealCard();
+	
+	public int calcBonus();
+	
+	public void placeUnits();
+	
 
 }
