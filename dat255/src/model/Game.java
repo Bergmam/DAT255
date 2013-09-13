15af1d6daa083be9f01b68147b0df601904902b1
LinneaAndersson <Linneamaria10@gmail.com>
2013-09-13 11:28:27 +0200

    added unimplemented methods in Game

--- dat255/src/model/Game.java
+++ dat255/src/model/Game.java
@@ -7,4 +7,46 @@ package model;
 
 public class Game implements GameInterface{
 
+	@Override
+	public void changePhase() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changeTurn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void attack(Province offensiv, Province defensive) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Player getActivePlayer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void dealCard() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int calcBonus() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void placeUnits() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
