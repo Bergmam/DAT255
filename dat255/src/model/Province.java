e228a7d2da518bdec883f16fa9b4abedf3ef223e
LinneaAndersson <Linneamaria10@gmail.com>
2013-09-13 10:43:20 +0200

    added methds and cnstructor to Province

--- dat255/src/model/Province.java
+++ dat255/src/model/Province.java
@@ -6,5 +6,27 @@ package model;
  * 
  */
 public class Province {
-
+	private int units;
+	private String id;
+	
+	public Province(String province){
+		id=province;
+	}
+	
+	public int getUnits(){
+		return units;
+	}
+	
+	public void addUnits(int units){
+		this.units += units; 
+	}
+	
+	public void moveUnits(int units, Province province){
+		this.units-=units;
+		province.addUnits(units);
+	}
+	
+	public String getId(){
+		return id;
+	}
 }
