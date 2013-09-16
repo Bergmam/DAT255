fcf288267df31f8aa443415c1b82799345577112
NastyFever <k-lime@hotmail.com>
2013-09-16 11:29:43 +0200

    Knopat vidare

--- dat255/src/model/Player.java
+++ dat255/src/model/Player.java
@@ -12,7 +12,7 @@ import model.DecksOfCards.CardType;
 
 public class Player {
 	String name;
-	int turnId, nrOfInf=0, nrOfCav=0, nrOfArt=0, nrOfWild=0;
+	int turnId;
 	ArrayList<CardType> cards; // The cards the player currently has on his/her hand.
 	
 	public Player(int turnId, String name){
@@ -25,15 +25,26 @@ public class Player {
 		cards.add(DecksOfCards.takeCard());
 	}
 	
-	private void removeCard(CardType card){
-		cards.remove(card);
+	private void removeCard(CardType c1, CardType c2, CardType c3){
+		cards.remove(c1); // Görs alltid x3
+		cards.remove(c2); 
+		cards.remove(c3); 		
+		DecksOfCards.discardCard(c1);
+		DecksOfCards.discardCard(c2);
+		DecksOfCards.discardCard(c3);
 	}
 
 	/*
-	 * Oklart hur den ska implementeras, men mitt förslag är att det
-	 * finns ints som håller reda på hur många av varje kort det finns.
+	 * Vidare är inte provinces knutna till korten.
 	 */
-	public boolean exchangeCard(CardType card){
+	public boolean exchangeCard(CardType c1, CardType c2, CardType c3){
+		if(c1.equals(c2) && c2.equals(c3)){ // 3 Lika, dock får vi Wildcardbug
+			removeCard(c1, c2, c3);
+			return true;
+		}
+		else if(!(c1.equals(c2)) && !(c2.equals(c3)) && !(c1.equals(c3))){ // En av varje
+			
+		}
 		return false;
 	}
 	/*
