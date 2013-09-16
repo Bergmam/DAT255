5915d21cbe2ce05f61b4ac9a2ef0d0b48ed0e2ea
NastyFever <k-lime@hotmail.com>
2013-09-16 11:54:21 +0200

    Utvecklat builden för DecksofCard
    Samt kommenterat kring om det är värt att skaffa en till klass för Card

--- dat255/src/model/DecksOfCards.java
+++ dat255/src/model/DecksOfCards.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -13,9 +14,31 @@ public class DecksOfCards {
 
 
 	private static LinkedList<CardType> cardDeck;
-	private static List<CardType> discardPile;
+	private static LinkedList<CardType> discardPile;
 
 	public DecksOfCards(ArrayList<String> provinces){
+		int infantry=14; //Antaget värde hittade inget i reglerna
+		int cavalry=14; //Antaget värde hittade inget i reglerna
+		/*
+		 * Förutsätter att rätt antal provincer finns, inte generell lösning.
+		 */
+		for(String p:provinces){
+			if (infantry>0){
+			//discardPile.add(new Card(p), INFANTRY);
+			infantry--;
+			}
+			else if(cavalry>0){
+				//discardPile.add(new Card(p), CAVALRY);
+				cavalry--;
+			}
+			else{
+				//discardPile.add(new Card(p), ARTILLERY);
+			}
+		}
+		for(int i=0; i<3 ;i++){
+			//discardPile.add(new Card(null), WILDCARD);
+		}
+		
 	}
 	
 	public static CardType takeCard(){
@@ -27,7 +50,9 @@ public class DecksOfCards {
 	}
 	
 	private void shuffle(){
-		
+		Collections.shuffle(discardPile);
+		cardDeck=discardPile;
+		discardPile=null;
 	}
 	enum CardType{Infantry, Artillery, Cavalry, WildCard};
 	
