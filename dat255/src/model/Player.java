b9ea769af9f458d284fbf08e162d37e753d10a4e
NastyFever <k-lime@hotmail.com>
2013-09-16 10:41:01 +0200

    Små tillägg till Player

--- dat255/src/model/Player.java
+++ dat255/src/model/Player.java
@@ -12,18 +12,30 @@ import model.DecksOfCards.CardType;
 
 public class Player {
 	String name;
-	int turnId;
+	int turnId, nrOfInf=0, nrOfCav=0, nrOfArt=0, nrOfWild=0;
 	ArrayList<CardType> cards; // The cards the player currently has on his/her hand.
 	
 	public Player(int turnId, String name){
 		this.turnId = turnId;
 		this.name = name;
+		
 	}
 	
 	public void addCard(){
 		cards.add(DecksOfCards.takeCard());
 	}
 	
+	private void removeCard(CardType card){
+		cards.remove(card);
+	}
+
+	/*
+	 * Oklart hur den ska implementeras, men mitt förslag är att det
+	 * finns ints som håller reda på hur många av varje kort det finns.
+	 */
+	public boolean exchangeCard(CardType card){
+		return false;
+	}
 	/*
 	 * Test test 
 	 */
