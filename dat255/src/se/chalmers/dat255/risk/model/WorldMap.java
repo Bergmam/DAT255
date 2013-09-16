a47890e019a9a91a7df15f975a31ead5948eda5b
NastyFever <k-lime@hotmail.com>
2013-09-16 16:41:26 +0200

    Grejat lite
    funkar inte så bra java är bortglömt

--- dat255/src/se/chalmers/dat255/risk/model/WorldMap.java
+++ dat255/src/se/chalmers/dat255/risk/model/WorldMap.java
@@ -3,6 +3,9 @@ package se.chalmers.dat255.risk.model;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 import android.util.Pair;
 
@@ -21,7 +24,27 @@ public class WorldMap {
 	 * L�sa in hela filen h�r?
 	 */
 	public WorldMap(File file){
-		
+        try {
+            //
+            // Create a new Scanner object which will read the data 
+            // from the file passed in. To check if there are more 
+            // line to read from it we check by calling the 
+            // scanner.hasNextLine() method. We then read line one 
+            // by one till all line is read.
+            //
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] array = line.split("-");
+                String p1 = array[0];
+                for(int i=1; i<array.length(); i++){
+                	String p2 = array[i];
+                	neighbours.put(new Pair<p1, p2>, true);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 	}
 	
 	public Player getOwner(String provinceName){
@@ -38,6 +61,8 @@ public class WorldMap {
 	
 	public boolean isNeighbours(String provinceName1, String provinceName2){
 		Pair<String,String> key = new Pair<String,String>(provinceName1, provinceName2);
-		return neighbours.get(key);
+		if(neighbours.contains(key))
+			return true;
+		return false;
 	}
 }
