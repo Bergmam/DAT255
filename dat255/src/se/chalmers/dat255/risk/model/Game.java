d3f6817ab1cb3dcc5c242623c5cc3e94a3085393
LinneaAndersson <Linneamaria10@gmail.com>
2013-09-16 16:26:34 +0200

    Implementing methods in Game

--- dat255/src/se/chalmers/dat255/risk/model/Game.java
+++ dat255/src/se/chalmers/dat255/risk/model/Game.java
@@ -1,28 +1,43 @@
 package se.chalmers.dat255.risk.model;
+
 /**
- * The top game class. Controls flow between our lower classes,
- * such as the battle handler and the WorldMap.
- *
+ * The top game class. Controls flow between our lower classes, such as the
+ * battle handler and the WorldMap.
+ * 
  */
 
-public class Game implements GameInterface{
+public class Game implements GameInterface {
+	private Player[] players;
+	private int activePlayer;
+	private int currentPhase;
+	
+	/**
+	 * Creates a new Game.
+	 * @param playersId The ids of the players
+	 */
+	public Game(String[] playersId) {
+		for(int i = 0; i < playersId.length ; i++){
+			players[i]= new Player(i, playersId[i]);
+		}
+		currentPhase=1;
+	}
 
 	@Override
 	public void changePhase() {
-		// TODO Auto-generated method stub
-		
+		currentPhase++;
 	}
 
 	@Override
 	public void changeTurn() {
-		// TODO Auto-generated method stub
-		
+		// Check this!
+		activePlayer = (activePlayer+1)%players.length;
+		currentPhase = 1;
 	}
 
 	@Override
 	public void attack(Province offensiv, Province defensive) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -34,7 +49,7 @@ public class Game implements GameInterface{
 	@Override
 	public void dealCard() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -46,7 +61,7 @@ public class Game implements GameInterface{
 	@Override
 	public void placeBonusUnits(int units, Province province) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -54,6 +69,5 @@ public class Game implements GameInterface{
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
 
 }
