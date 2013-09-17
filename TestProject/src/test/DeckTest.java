3b3d1bbd66fc7c5d6b3be4e18aa21333e1d4b948
Karamech <emma@goding.eu>
2013-09-17 11:18:40 +0200

    Created junit for Deck

---
+++ TestProject/src/test/DeckTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DeckTest {
+
+	@Test
+	public void testDeck() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGiveCard() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testDiscard() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRecycleCards() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
