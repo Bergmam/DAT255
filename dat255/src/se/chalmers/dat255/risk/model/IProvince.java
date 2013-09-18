f8e5f77f5fa59067461f171c8b9c7377f7e8321f
Andreas <andreas.wahlstrom@hotmail.com>
2013-09-18 10:55:20 +0200

    Wrote JavaDoc for IProvince

--- dat255/src/se/chalmers/dat255/risk/model/IProvince.java
+++ dat255/src/se/chalmers/dat255/risk/model/IProvince.java
@@ -1,18 +1,51 @@
 package se.chalmers.dat255.risk.model;
 
+/**
+ * Interface for provinces.
+ * 
+ */
 public interface IProvince {
 
-	public int getUnits();
-	
-	public void addUnits(int units);
-	
-	public void removeUnits(int units);
-	
 	/**
-	 * Used in F2 after gaining a province and in F3. 
+	 * Getter for units in the province
+	 * 
+	 * @return the number of units in the province
+	 */
+	public int getUnits();
+
+	/**
+	 * Adds units to this province
+	 * 
+	 * @param units
+	 *            , number of units to be added
+	 */
+	public void addUnits(int units);
+
+	/**
+	 * Removes units from this province
+	 * 
+	 * @param units
+	 *            , number of units to be removed
+	 */
+	public void removeUnits(int units);
+
+	/**
+	 * Move units forom this province to another province. Used in F2 after
+	 * gaining a province and in F3.
+	 * 
+	 * @param units
+	 *            , number of units to be moved
+	 * 
+	 * @param province
+	 *            , the province to receive the units
 	 */
 	public void moveUnits(int units, IProvince province);
-	
+
+	/**
+	 * Fetches the string identification of this province
+	 * 
+	 * @return the name(ID) of this province
+	 */
 	public String getId();
 
 }
