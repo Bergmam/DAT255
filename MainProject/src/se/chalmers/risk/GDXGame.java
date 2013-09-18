1cb9a49c4af7ef1fbdc1173caa5e1d256765278f
Andreas <andreas.wahlstrom@hotmail.com>
2013-09-18 19:37:59 +0200

    added fps logger and green screen

--- MainProject/src/se/chalmers/risk/GDXGame.java
+++ MainProject/src/se/chalmers/risk/GDXGame.java
@@ -1,43 +1,52 @@
 package se.chalmers.risk;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.FPSLogger;
+import com.badlogic.gdx.graphics.GL20;
 
-public class GDXGame  implements ApplicationListener {
+public class GDXGame implements ApplicationListener {
+
+	FPSLogger logger;
 
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
-		
+
+		Gdx.app.log("Risk", "creating game");
+
+		logger = new FPSLogger();
+
 	}
 
 	@Override
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void render() {
-		// TODO Auto-generated method stub
-		
+		Gdx.app.log("Risk", "Render");
+		Gdx.gl.glClearColor(0f, 1f, 0f, 1f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 	}
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
-		
+		Gdx.app.log("Risk", "Pausing game");
 	}
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
-		
+		Gdx.app.log("Risk", "Resuming game");
+
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-		
+		Gdx.app.log("Risk", "Destroying game");
+
 	}
 
 }
