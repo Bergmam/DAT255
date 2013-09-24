8e9be12810550717e042bf1e1844f8f807c626b1
LinneaAndersson <Linneamaria10@gmail.com>
2013-09-24 11:02:26 +0200

    added a comment

--- MainProject/src/se/chalmers/dat255/risk/view/CardStage.java
+++ MainProject/src/se/chalmers/dat255/risk/view/CardStage.java
@@ -17,6 +17,10 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
  * 
  * Perhaps receiving all card player belonging to a player in the property
  * change and refresh all cardViews then?
+ * 
+ * I think we will add CardViews when player gets more card, 
+ * and when using them we will destroy the cardViews. So we will get a propertyChange when
+ * player removes this card from the hand.
  */
 public class CardStage extends Stage implements PropertyChangeListener {
 	private List<CardView> actor;
