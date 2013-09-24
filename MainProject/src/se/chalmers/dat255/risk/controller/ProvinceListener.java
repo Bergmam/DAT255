8471152a1fc97b1a88c5ca6d595dc80d8eff0f67
NastyFever <k-lime@hotmail.com>
2013-09-24 10:55:02 +0200

    Added cotroller logics for F3 movingTroops

--- MainProject/src/se/chalmers/dat255/risk/controller/ProvinceListener.java
+++ MainProject/src/se/chalmers/dat255/risk/controller/ProvinceListener.java
@@ -1,5 +1,7 @@
 package se.chalmers.dat255.risk.controller;
 
+import se.chalmers.dat255.risk.model.IGame;
+import se.chalmers.dat255.risk.model.IProvince;
 import se.chalmers.dat255.risk.view.ProvinceView;
 
 import com.badlogic.gdx.Gdx;
@@ -8,13 +10,41 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class ProvinceListener extends ClickListener {
 
+	IProvince oldClickedProvince;
+	IProvince movingFrom;//Used for F3 while moving troops
+	IGame	theGame;
+	boolean movingTroops;//If in F3 and already choosed from where to where to move troops
 	@Override
 	public void clicked(InputEvent event, float x, float y){
 		if(event.getTarget() instanceof ProvinceView){
-			((ProvinceView)event.getTarget()).check();
-			((ProvinceView)event.getTarget()).addUnits();
+			ProvinceView newClick =  ((ProvinceView)event.getTarget());
+			IProvince newClickedProvince = newClick.getProvince();
+			
+			newClick.check();
+	//		newClick.addUnits();
+			//if(F1)
+//			((ProvinceView)event.getTarget()).addUnits();
+			if(movingTroops){
+				if(newClickedProvince==oldClickedProvince){
+					theGame.moveToProvince(1, movingFrom, oldClickedProvince); //I nuläget flyttas endast en trupp
+				}
+				else{
+					movingTroops=false;
+					movingFrom=null;
+					oldClickedProvince=null;
+				}
+			}
+			else if(oldClickedProvince!=null){
+					if(oldClickedProvince!=newClickedProvince){
+						if(theGame.moveToProvince(1, oldClickedProvince, newClickedProvince)){
+							movingTroops=true;
+						} //I nuläget flyttas endast en trupp
+					}
+			}
+			oldClickedProvince=newClickedProvince;
 		}
 		Gdx.app.log("trololol", "province clicked");
+		
 	}
 
 }
