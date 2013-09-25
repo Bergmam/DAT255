617f7465b801505dc734bb455a0a0c55eafea667
NastyFever <k-lime@hotmail.com>
2013-09-25 15:46:58 +0200

    Fix'd CardListener

--- MainProject/src/se/chalmers/dat255/risk/controller/CardListener.java
+++ MainProject/src/se/chalmers/dat255/risk/controller/CardListener.java
@@ -1,20 +1,24 @@
 package se.chalmers.dat255.risk.controller;
 
 import se.chalmers.dat255.risk.model.IProvince;
-import se.chalmers.dat255.risk.view.ProvinceView;
+import se.chalmers.dat255.risk.model.ICard;
+import se.chalmers.dat255.risk.model.IGame;
+import se.chalmers.dat255.risk.view.CardView;
 
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 //import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
+
 public class CardListener extends ClickListener {
+	IGame theGame;
 	@Override
 	public void clicked(InputEvent event, float x, float y){
-		if(event.getTarget() instanceof ProvinceView){
-			ProvinceView newClick =  ((ProvinceView)event.getTarget());
-			IProvince newClickedProvince = newClick.getProvince();
+		if(event.getTarget() instanceof CardView){
+			CardView newClick =  ((CardView)event.getTarget());
+			ICard newClickedCard = newClick.getCard();
 			newClick.check();
-			theGame.handleProvinceClick(newClickedProvince);
+			theGame.handleCardClick(newClickedCard);
 		}
 	//	Gdx.app.log("trololol", "province clicked");
 	}
