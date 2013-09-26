02d3f5954292ff325417d5ca24365a38b3f4cb65
LinneaAndersson <Linneamaria10@gmail.com>
2013-09-26 15:11:54 +0200

    fixed eckTesst

--- TestProject/src/test/PlayerTest.java
+++ TestProject/src/test/PlayerTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 
 import org.junit.Test;
 
+import se.chalmers.dat255.risk.model.Card;
 import se.chalmers.dat255.risk.model.Deck;
 import se.chalmers.dat255.risk.model.ICard;
 import se.chalmers.dat255.risk.model.Player;
@@ -19,7 +20,8 @@ public class PlayerTest {
 		provinces.add("Hej");
 		provinces.add("Hejhopp");
 		provinces.add("Hejmamma");
-		Deck deck = Deck.getInstance(provinces, 3);
+		Deck deck = Deck.getInstance();
+		deck.CreateCards(provinces, 3);
 		Player player = new Player(1, "Testare");
 		player.addCard();
 		assertTrue(player.getCards().size() == 1);
@@ -42,15 +44,33 @@ public class PlayerTest {
 		provinces.add("Hej");
 		provinces.add("Hejhopp");
 		provinces.add("Hejmamma");
-		Deck deck = Deck.getInstance(provinces, 2);
+		Deck deck = Deck.getInstance();
+		deck.CreateCards(provinces, 0);
 		Player player = new Player(1, "Testare");
 		player.addCard();
 		player.addCard();
 		player.addCard();
-		ICard card1 = player.getCards().get(2);
-		ICard card2 = player.getCards().get(1);
-		ICard card3 = player.getCards().get(0);
+		
+		ICard card1 = new Card(Card.CardType.ARTILLERY, "a");
+		ICard card2 = new Card(Card.CardType.ARTILLERY, "b");
+		ICard card3 = new Card(Card.CardType.ARTILLERY, "c");
+		
+		ICard card4 = new Card(Card.CardType.CAVALRY, "d");
+		ICard card5 = new Card(Card.CardType.CAVALRY, "e");
+		ICard card6 = new Card(Card.CardType.JOKER, "f");
+		
+		ICard card7 = new Card(Card.CardType.INFANTRY, "g");
+		ICard card8 = new Card(Card.CardType.INFANTRY, "h");
+		ICard card9 = new Card(Card.CardType.INFANTRY, "e");
+		
+		ICard card10 = new Card(Card.CardType.JOKER, "ff");
+		ICard card11 = new Card(Card.CardType.JOKER, "hg");
+		ICard card12 = new Card(Card.CardType.ARTILLERY, "dd");
+		
 		assertTrue(player.exchangeCard(card1, card2, card3));
+		assertTrue(player.exchangeCard(card4, card5, card6));
+		assertTrue(!player.exchangeCard(card10, card11, card12));
+		
 	}
 
 }
