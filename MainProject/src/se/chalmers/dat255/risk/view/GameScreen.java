fc36e37345bc3a093a2bbcaafe05bfafdd219bdc
LinneaAndersson <Linneamaria10@gmail.com>
2013-09-27 16:30:41 +0200

    working on txt-fie

--- MainProject/src/se/chalmers/dat255/risk/view/GameScreen.java
+++ MainProject/src/se/chalmers/dat255/risk/view/GameScreen.java
@@ -30,7 +30,7 @@ public class GameScreen extends AbstractScreen {
 
 		isWorld = true;
 
-		worldStage = new WorldStage(model.getGameProvinces(), new File("Gfx/Cords.txt"));
+		worldStage = new WorldStage(model.getGameProvinces(), Gdx.files.internal("Gfx/Cords.txt").file());
 
 		// Creates a cardStage for every player
 		cardStages = new ArrayList<AbstractStage>();
