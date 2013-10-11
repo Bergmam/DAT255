bffd3f17286624922326e4bac45c1190ff937476
LinneaAndersson <Linneamaria10@gmail.com>
2013-10-11 15:08:00 +0200

    orking on calcBonus test

--- TestProject/src/test/BonusHandlerTest.java
+++ TestProject/src/test/BonusHandlerTest.java
@@ -13,25 +13,74 @@ import se.chalmers.dat255.risk.model.WorldMap;
 
 public class BonusHandlerTest {
 	ArrayList<Player> players = new ArrayList();
+	int startingTroopNumber;
 	String provinces = "A-B-C-D-E" + "\n" + "B-A-C-E" + "\n" + "C-A-D" + "\n"
 			+ "D-A-C-E" + "\n" + "E-A-B-D";
-	String continents = "3-continent1-A-B-C" + "\n" + "2-continent2-D-E";
+	int continentBonus1 = 3;
+	String continents = continentBonus1 + "-continent1-A" + "\n"
+			+ "5-continent2-B-C-D-E";
 	WorldMap worldMap;
 	BonusHandler bH;
-	
+	int miniumBonus = 3;
+
 	@Before
 	public void beforeTest() {
 		players.add(new Player(0, "Linnea"));
 		players.add(new Player(1, "Andreas"));
 		worldMap = new WorldMap(provinces, continents, players);
 		bH = new BonusHandler(worldMap, players.size());
+		startingTroopNumber = (50 - players.size() * 5);
 	}
-	
+
 	@Test
-	public void testBonusFromStart(){
-		assertTrue(bH.getBonus()==0);
+	public void testBonusFromStart() {
+		assertTrue(bH.getBonus() == 0);
 		bH.calcBonusForF0(worldMap.getProvinces().size());
-		assertTrue(bH.getBonus() == (50 - players.size() * 5)-worldMap.getProvinces().size());
+		assertTrue(bH.getBonus() == startingTroopNumber
+				- worldMap.getProvinces().size());
+	}
+
+	@Test
+	public void testCalcBonus() {
+		int numberOfProvinces = players.get(0).getNrOfProvinces();
+		int cBonusForPlayer1;
+		// Because one continent just have got one province, we know that player
+		// one or two has got one continent. And we also know that the other
+		// player havent got any continent, because the other continent contains
+		// 4 provinces.
+
+		// Both has minimum bonus for provinces, because there is just 5
+		// provinces now.
+		bH.calcBonusUnits(players.get(0));
+		System.out.println("player1 bonus = " + bH.getBonus());
+		if (worldMap.getOwner("A") == players.get(0)) {
+			System.out.println("in player1 has A " + (continentBonus1 + miniumBonus) + " == " + bH.getBonus());
+			cBonusForPlayer1 = continentBonus1;
+			assertTrue(bH.getBonus() == continentBonus1 + miniumBonus);
+			bH.calcBonusUnits(players.get(1));
+			assertTrue(bH.getBonus() == miniumBonus);
+		} else {
+			System.out.println("in player2 has A");
+			cBonusForPlayer1=0;
+			assertTrue(bH.getBonus() == miniumBonus);
+			bH.calcBonusUnits(players.get(1));
+			assertTrue(bH.getBonus() == continentBonus1 + miniumBonus);
+		}
+
+		// We add some provinces to players.get(0) to see if the bonus
+		// increments.
+		while (players.get(0).getNrOfProvinces() < 11) {
+			players.get(0).gainProvince();
+			bH.calcBonusUnits(players.get(0));
+			assertTrue(bH.getBonus() == miniumBonus + cBonusForPlayer1);
+		}
+		
+		while (players.get(0).getNrOfProvinces() < 42) {
+			players.get(0).gainProvince();
+			bH.calcBonusUnits(players.get(0));
+			assertTrue(bH.getBonus() == players.get(0).getNrOfProvinces() / 3 + cBonusForPlayer1);
+		}
+
 	}
 
 }
