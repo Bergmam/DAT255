c7ce8ff51fdd6e184fd3920fc1ec4e2c250c091e
LinneaAndersson <Linneamaria10@gmail.com>
2013-10-20 15:19:25 +0200

    fixed checkUserName and numberofplayers

--- dat255/src/se/chalmers/dat255/risk/activity/CreateGameActivity.java
+++ dat255/src/se/chalmers/dat255/risk/activity/CreateGameActivity.java
@@ -17,59 +17,60 @@ public class CreateGameActivity extends Activity {
 
 	private String[] spinnerItems;
 	private OnTouchListener startListener;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.startgame_layout);
-		
-		spinnerItems=new String[5];
-        spinnerItems[0]="1";
-        spinnerItems[1]="2";
-        spinnerItems[2]="3";
-        spinnerItems[3]="4";
-        spinnerItems[4]="5";
-        Spinner s = (Spinner) findViewById(R.id.spinner);
-        ArrayAdapter adapter = new ArrayAdapter(this,
-        android.R.layout.simple_spinner_item, spinnerItems);
-        s.setAdapter(adapter);
-        
-        final EditText nameField = (EditText) findViewById(R.id.nameField);
-        Button startButton = (Button) findViewById(R.id.startbutton);
-        
-        startListener = new OnTouchListener() {
-			
-	    	@Override
-	        public boolean onTouch(View v, MotionEvent event) {
-	          switch (event.getAction() & MotionEvent.ACTION_MASK) {
-	          case MotionEvent.ACTION_DOWN:
-	        	  	//TODO
-	        	  String username = nameField.getText().toString();
-	        	  User user;
-	        	  
-	        	  if(checkUsername(username)){
-	        		  //Create lobby activity
-	        		  //Set max players to spinner value (intent)
-	        		  user = new User(username);
-	        		  
-	        		  Intent intent = new Intent("NetworkHandler.intent.action.Launch");
-		        	  intent.putExtra("user", username);
-		        	  intent.putExtra("game", "ThisisagameFYI");
-		        	  startActivity(intent);
-	        	  }
 
-	        	  return true;
-	          case MotionEvent.ACTION_UP:
-	            return true;
-	          default:
-	            return false;
-	          }
-	        }
-	      };
-        
-	      startButton.setOnTouchListener(startListener);
+		spinnerItems=new String[4];
+		spinnerItems[0]="3";
+		spinnerItems[1]="4";
+		spinnerItems[2]="5";
+		spinnerItems[3]="6";
+		Spinner s = (Spinner) findViewById(R.id.spinner);
+		ArrayAdapter adapter = new ArrayAdapter(this,
+				android.R.layout.simple_spinner_item, spinnerItems);
+		s.setAdapter(adapter);
+
+		final EditText nameField = (EditText) findViewById(R.id.nameField);
+		Button startButton = (Button) findViewById(R.id.startbutton);
+
+		startListener = new OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				switch (event.getAction() & MotionEvent.ACTION_MASK) {
+				case MotionEvent.ACTION_DOWN:
+					//TODO
+					String username = nameField.getText().toString();
+					User user;
+
+					if(checkUsername(username)){
+						//Create lobby activity
+						//Set max players to spinner value (intent)
+						user = new User(username);
+
+						Intent intent = new Intent("NetworkHandler.intent.action.Launch");
+						intent.putExtra("user", username);
+						intent.putExtra("game", "ThisisagameFYI");
+						startActivity(intent);
+					}
+
+					return true;
+				case MotionEvent.ACTION_UP:
+					return true;
+				default:
+					return false;
+				}
+			}
+		};
+
+		startButton.setOnTouchListener(startListener);
 	}
 	private boolean checkUsername(String name){
-		return true;
+		if(name.length()>1 && name.length()<15)
+			return true;
+		return false;
 	}
 }
